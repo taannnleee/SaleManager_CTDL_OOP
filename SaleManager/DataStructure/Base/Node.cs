6ffb2fe5ff09609dc175b9df5319b54b32d59012
w3schools-test <116615872+taannnleee@users.noreply.github.com>
2023-06-23 22:21:35 +0700

    base develop-tt

---
+++ SaleManager/DataStructure/Base/Node.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaleManager.DataStructure.Base
+{
+    public class Node<T>
+    {
+        private T item;
+        private Node<T>? next;
+        private Node<T>? previous;
+
+        public Node(T value)
+        {
+            this.item = value;
+            next = null; 
+            previous = null;
+        }
+    }
+}
